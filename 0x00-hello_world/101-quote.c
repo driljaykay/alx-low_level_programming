2b7251de9453f6430cdc9d0b6cfeaa797c729dbd
driljaykay <johnjaykay@yahoo.com>
2023-02-17 03:06:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fputs("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19\n", stdout);
+return (1);
 }
 
