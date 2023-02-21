fdc0ce765b1ddd6246203629419dea26960b5fbf
driljaykay <johnjaykay@yahoo.com>
2023-02-20 20:57:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - prints "and that piece of art is useful"- Dora Korpar, 2015-10-19",
+ * Return: always 1
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
-
