36880f3695ca4e3038f03e6e61e557afa17c9eb4
driljaykay <johnjaykay@yahoo.com>
2023-02-23 23:22:53 -0800

    7-diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
@@ -20,10 +20,10 @@ void print_diagonal(int n)
 		{
 			for (q = 0; q < p; q++)
 			{
-				putchar(32);
+				_putchar(32);
 			}
-			putchar(92);
-			putchar('\n');
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 }
