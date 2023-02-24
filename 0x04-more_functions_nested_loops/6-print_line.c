4c29765eaf391fa73f08a6edadc3cb229875b654
driljaykay <johnjaykay@yahoo.com>
2023-02-23 23:20:17 -0800

    6-lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,14 +12,14 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			putchar(95);
+			_putchar(95);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
