97c067b79811e08cc3f965c0993d7f6a905037b0
driljaykay <johnjaykay@yahoo.com>
2023-02-27 13:50:55 -0800

    I do not fear

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * followed by a new line, to stdout
+ * @str: string to be print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
