2984ec615a999f1b108b51daa7ce98cfa1b12700
driljaykay <johnjaykay@yahoo.com>
2023-03-02 13:16:05 -0800

    optional

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,4 +9,42 @@
  */
 
 void print_buffer(char *b, int size)
+{
+	int a, d, c;
 
+	a = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (a < size)
+	{
+		d = size - a < 10 ? size - a : 10;
+		printf("%08x: ", a);
+		for (c = 0; c < 10; c++)
+		{
+			if (c < d)
+				printf("%02x", *(b + a + c));
+			else
+				printf(" ");
+			if (c % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (c = 0; c < d; c++)
+		{
+			int e = *(b + a + c);
+
+			if (e < 32 || e > 132)
+			{
+				e = '.';
+			}
+			printf("%c", e);
+		}
+		printf("\n");
+		a += 10;
+	}
+}
