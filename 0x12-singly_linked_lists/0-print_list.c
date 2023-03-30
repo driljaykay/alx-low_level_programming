a05995ad9a5100a8e80d4a1fdc6fadab20146a94
driljaykay <johnjaykay@yahoo.com>
2023-03-30 05:12:14 -0700

    Singly linked lists Tasks all updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		nodes++;
 	}
-	return (s);
+	return (nodes);
 }
