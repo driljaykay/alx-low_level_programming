58c0712ca1995a139a973c8b42a633f1edad7e7c
driljaykay <johnjaykay@yahoo.com>
2023-04-10 14:36:07 -0700

    C - Bit manipulation Tasks 0 to 5 completed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
 /**
  * get_bit - returns the value of a bit at a given index
  * @n: the number whose bit value is to be returned
- * @index: the index of the bit to be returned
+ * @index: the index, starting from 0 of the bit you want to get
  *
  * Return: the value of the bit at index index or -1 if an error occured
  */
