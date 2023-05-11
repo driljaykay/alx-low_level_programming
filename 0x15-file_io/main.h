4017a7d5c11b4f37cbe9c23f074cb604654ca289
driljaykay <driljaykay@gmail.com>
2023-05-11 07:19:18 -0700

    main header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,10 +7,11 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+int copy_content_from_one_to_another(char *file_from, char *file_to);
 #endif
