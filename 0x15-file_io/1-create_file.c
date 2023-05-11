15f495b1b9ad87aeea97016a3eee35704c16751e
driljaykay <driljaykay@gmail.com>
2023-05-11 07:49:13 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (file_descriptor == failure)
 		return (failure);
@@ -40,9 +40,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (res_wr_fcn == failure)
 		return (failure);
-
-	if (chmod(filename, 0600) == -1)
-		return (-1);
 
 	return (success);
 }
