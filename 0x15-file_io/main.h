50cc6e7c0fdcd052c1d66328f48dc94fea6227c2
driljaykay <driljaykay@gmail.com>
2023-05-11 07:22:34 -0700

    main header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_content_from_one_to_another(char *file_from, char *file_to);
+int write_content_of_file(char *filename, char *buffer, ssize_t length);
+ssize_t read_content_of_file(char *filename, char *buffer);
+
 #endif
