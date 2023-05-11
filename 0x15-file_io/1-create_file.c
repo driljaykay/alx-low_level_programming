ed49931740d207aafffeaa3ea1ff82f80f05b8c6
driljaykay <driljaykay@gmail.com>
2023-05-11 07:55:12 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 	if (res_wr_fcn == failure)
 		return (failure);
 
-	if (chmod(filename, 0600) == failure)
+	if (chmod(filename, S_IRUSR | S_IWUSR) == failure)
 		return (failure);
 
 	return (success);
